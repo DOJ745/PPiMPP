de1df0f1a07f81f8bc6b5f0b8115061536a46785
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-11-15 15:14:45 +0300

    Need to create controllers

--- LB9/Assets/Scripts/CubeController.cs
+++ LB9/Assets/Scripts/CubeController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeController : MonoBehaviour
 {
+    private static int points = 0;
+
     public float maxSpeed = 10f;
     public float jumpForce = 700f;
 
@@ -15,10 +18,24 @@ public class CubeController : MonoBehaviour
     public LayerMask whatIsGround;
 
     public float move;
+    public Text score;
 
     private Rigidbody2D rigidBody;
     // Start is called before the first frame update
-
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider == true)
+        {
+            SoundManagerScript.PlaySound("walking");
+        }
+        if(coll.collider.gameObject.name == "Coin")
+        {
+            SoundManagerScript.PlaySound("getCoin");
+            Destroy(coll.collider.gameObject);
+            points++;
+            PlayerScore(points);
+        }
+    }
 
     void Start()
     {
@@ -52,18 +69,25 @@ public class CubeController : MonoBehaviour
         }
             
 
-        if (Input.GetKey(KeyCode.Escape))
+        if (points == 5)
         {
+            score.text = "You collected all coins!";
             Application.Quit();
         }
 
         void Flip()
         {
-            SoundManagerScript.PlaySound("walking");
             facingRight = !facingRight;
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
         }
+    }
+
+
+    private void PlayerScore(int point)
+    {
+        Debug.Log("Gained point!");
+        score.text = "Score: " + point;
     }
 }
