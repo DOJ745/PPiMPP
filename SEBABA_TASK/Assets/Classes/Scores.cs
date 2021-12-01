9e41ff0251db805fed80b69fdb0004aa1562edab
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-12-01 20:01:33 +0300

    Testes scores serialization/deserialization in JSON

---
+++ SEBABA_TASK/Assets/Classes/Scores.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Scores
+{
+    public ArrayList scores { get; set; }
+
+    public Scores() 
+    { 
+        scores = new ArrayList();
+        fillScores(10);
+    }
+
+    public void saveScores()
+    {
+        string result = JsonUtility.ToJson(scores);
+    }
+
+    public ArrayList readScores(string collecttion)
+    {
+        Scores result = JsonUtility.FromJson<Scores>(collecttion);
+        return result.scores;
+    }
+
+    public void fillScores(int tableSize)
+    {
+        for (int i = 0; i < tableSize; i++)
+        {
+            scores.Add(100 + i);
+        }
+
+        scores.Sort(new ReverseSort());
+    }
+
+    public override string ToString()
+    {
+        string temp = "";
+
+        for (int i = 0; i < scores.Count; i++)
+        {
+            temp += scores[i] + "\n";
+        }
+
+        return temp;
+    }
+
+    private class ReverseSort : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return Comparer.Default.Compare(y, x);
+        }
+    }
+}
---
+++ SEBABA_TASK/Assets/Scripts/UI/ButtonsLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonsLogic : MonoBehaviour
+{
+    public Button startButton, highscoresButton, exitButton;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(startClick);
+        highscoresButton.onClick.AddListener(highscoresClick);
+        exitButton.onClick.AddListener(exitClick);
+    }
+
+    public void startClick()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void highscoresClick()
+    {
+        Scores test = new Scores();
+        Debug.Log("You have clicked the button!");
+        Debug.Log(test.ToString());
+    }
+
+    public void exitClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
+}
