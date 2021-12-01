91821726078c4909ace5936a4dc440a88989078e
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-12-01 13:32:21 +0300

    Can detect face and eyes, but problems with mask

--- LB12_16/Laba11-1/MAIN.cpp
+++ LB12_16/Laba11-1/MAIN.cpp
@@ -76,6 +76,71 @@ void cornerHarris_demo(int, void*)
     imshow(corners_window, dst_norm_scaled);
 }
 
+CascadeClassifier faceCascade, eyesCascade, mouthCascade;
+
+string faceCascadeName = "frontal_face2.xml";
+string eyesCascadeName = "eyes.xml";
+string mouthCascadeName = "mouth.xml";
+
+void detectAndDisplay(Mat frame);
+void detectAndDisplay(Mat frame)
+{
+
+    Mat frame_gray;
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+
+    equalizeHist(frame_gray, frame_gray);
+
+    vector<Rect> faces;
+    faceCascade.detectMultiScale(frame_gray, faces);
+
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+
+        ellipse(frame, 
+            center, 
+            Size(faces[i].width / 2, faces[i].height / 2), 
+            0, 0, 360, Scalar(0, 0, 255), 4);
+
+        Mat faceROI = frame_gray(faces[i]);
+
+        vector<Rect> eyes;
+        eyesCascade.detectMultiScale(faceROI, eyes);
+
+        for (size_t j = 0; j < eyes.size(); j++)
+        {
+            Point eye_center(
+                faces[i].x + eyes[j].x + eyes[j].width / 2, 
+                faces[i].y + eyes[j].y + eyes[j].height / 2);
+
+            int radius = cvRound(
+                ((double)eyes[j].width + (double)eyes[j].height) * 0.25
+            );
+
+            circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
+        }
+
+        vector<Rect> mouths;
+        mouthCascade.detectMultiScale(faceROI, mouths);
+
+        for (size_t j = 0; j < mouths.size(); j++)
+        {
+            Point mouth_center(
+                faces[i].x + mouths[j].x + mouths[j].width / 2,
+                faces[i].y + mouths[j].y + mouths[j].height / 2);
+
+            int radius = cvRound(
+                ((double)mouths[j].width + (double)mouths[j].height) * 0.28
+            );
+
+            circle(frame, mouth_center, radius, Scalar(255, 55, 255), 2);
+        }
+    }
+    //-- Show what you got
+    imshow("Capture - Face detection", frame);
+}
+
 static void LB12() 
 {
     Mat srcImg = imread("beer.jpg", 1);
@@ -370,12 +435,7 @@ static void LB15()
 
 static void LB16() 
 {
-    // создание объект классификатора
-
-    CascadeClassifier faceCascade;
-    string faceCascadeName = "haarcascade_frontalface_alt.xml";
-    faceCascade.load(faceCascadeName);
-    // открытие видео с камеры
+    /*
     VideoCapture capture(0);
     Mat frame;
 
@@ -385,20 +445,24 @@ static void LB16()
     while (true)
     {
         capture >> frame;
-        // подготовка изображения
+
         Mat grayFrame;
         cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
         equalizeHist(grayFrame, grayFrame);
-        // обнаружение объектов
+
         vector<Rect> faces;
+        vector<Rect> eyes;
+
         faceCascade.detectMultiScale(grayFrame, faces, 1.1, 2, 0 |
             CASCADE_SCALE_IMAGE, Size(30, 30));
-        // перебор найденных объектов и отрисовка прямоугольников
+
         for (int i = 0; i < faces.size(); i++)
         {
             Point center(faces[i].x + faces[i].width / 2,
                 faces[i].y + faces[i].height / 2);
+
             Mat faceROI = grayFrame(faces[i]);
+
             for (i = 0; i < faces.size(); i++)
             {
                 rectangle(frame,
@@ -408,12 +472,37 @@ static void LB16()
                     CV_RGB(255, 0, 0), 2);
             }
         }
+
         imshow("video", frame);
         int key = waitKey(1);
-        if (key == 27/*ESC*/) break;
+        if (key == 27) break;
     }
 
-    waitKey();
+    waitKey();*/
+
+
+    faceCascade.load(faceCascadeName);
+    eyesCascade.load(eyesCascadeName);
+    mouthCascade.load(mouthCascadeName);
+
+    VideoCapture capture(0);
+
+    if (!capture.isOpened()) {
+        cerr << "Unable to open: " << endl;
+    }
+    Mat frame;
+    while (capture.read(frame))
+    {
+        if (frame.empty())
+        {
+            cout << "--(!) No captured frame -- Break!\n";
+            break;
+        }
+
+        detectAndDisplay(frame);
+
+        if (waitKey(10) == 27) { break; }
+    }
 }
 
 int main()
