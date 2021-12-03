b534ceea33f8a9dccd2b72f3ec033e42dd283347
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-12-03 22:21:45 +0300

    LB15 is done

--- LB15/LB15.py
+++ LB15/LB15.py
@@ -1,9 +1,10 @@
 # import the necessary packages
-from skimage import threshold_local
+from skimage.filters import threshold_local
 from transform import four_point_transform
 import argparse
 import numpy as np
 import cv2
+import imutils
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -40,27 +41,30 @@ for c in cnts:
 	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 	# if our approximated contour has four points, then we
 	# can assume that we have found our screen
-	if len(approx) == 4:
+	if len(approx) == 8:
 		screenCnt = approx
 		break
 # show the contour (outline) of the piece of paper
 print("STEP 2: Find contours of paper")
-cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 1)
 cv2.imshow("Outline", image)
 cv2.waitKey(0)
 
 # apply the four point transform to obtain a top-down
 # view of the original image
-warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+#warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+warped = four_point_transform(orig, screenCnt.reshape(8, 2) * ratio)
+
 # convert the warped image to grayscale, then threshold it
 # to give it that 'black and white' paper effect
-warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+#warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+
 T = threshold_local(warped, 11, offset = 10, method = "gaussian")
 warped = (warped > T).astype("uint8") * 255
 # show the original and scanned images
 print("STEP 3: Apply perspective transform")
-cv2.imshow("Original", imutils.resize(orig, height = 650))
-cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.imshow("Original", imutils.resize(orig, height = 700))
+cv2.imshow("Scanned", imutils.resize(warped, height = 700))
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
