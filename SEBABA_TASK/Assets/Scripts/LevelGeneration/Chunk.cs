5f824141a86d3ed2e9b2f341d03ebaadecb74e58
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-12-05 13:37:23 +0300

    Need to fix spawn of chunks

---
+++ SEBABA_TASK/Assets/Scripts/LevelGeneration/Chunk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    [Header("Points")]
+    public Transform begin;
+    public Transform end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
