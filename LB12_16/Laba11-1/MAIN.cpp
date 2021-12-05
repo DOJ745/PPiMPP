940a9baf87e540cd5b23ff55a2e24ea8769dba2a
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-12-05 19:41:34 +0300

    Did smth for LB17

--- LB12_16/Laba11-1/MAIN.cpp
+++ LB12_16/Laba11-1/MAIN.cpp
@@ -1,8 +1,14 @@
-﻿#include <opencv2/opencv.hpp>
+﻿#include <opencv2/core.hpp>
+
+#include <opencv2/opencv.hpp>
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 #include <opencv2/core/mat.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video.hpp>
+
+//#include <opencv2/optflow/motempl.hpp>
 
 #include <iostream>
 using namespace cv;
@@ -447,13 +453,135 @@ static void LB16()
     }
 }
 
+static void LB17_1()
+{
+    //create Background Subtractor objects
+    Ptr<BackgroundSubtractor> pBackSub;
+
+    pBackSub = createBackgroundSubtractorMOG2();
+    //pBackSub = createBackgroundSubtractorKNN();
+
+    VideoCapture capture(0);
+
+    if (!capture.isOpened()) {
+        cerr << "Unable to open: " << endl;
+    }
+
+    Mat frame, fgMask;
+    while (true) 
+    {
+        capture >> frame;
+        if (frame.empty())
+            break;
+
+        //update the background model
+        pBackSub->apply(frame, fgMask);
+        //get the frame number and write it on the current frame
+        rectangle(frame, cv::Point(10, 2), cv::Point(100, 20),
+            cv::Scalar(255, 255, 255), -1);
+        stringstream ss;
+
+        ss << capture.get(CAP_PROP_POS_FRAMES);
+        string frameNumberString = ss.str();
+
+        putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
+            FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+        //show the current frame and the fg masks
+        imshow("Frame", frame);
+        imshow("FG Mask", fgMask);
+        //get the input from the keyboard
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+    }
+
+}
+
+static void LB17_2() 
+{
+    VideoCapture capture(0);
+
+    if (!capture.isOpened()) {
+        cerr << "Unable to open: " << endl;
+    }
+    // Create some random colors
+    vector<Scalar> colors;
+    RNG rng;
+
+    for (int i = 0; i < 100; i++)
+    {
+        int r = rng.uniform(0, 256);
+        int g = rng.uniform(0, 256);
+        int b = rng.uniform(0, 256);
+        colors.push_back(Scalar(r, g, b));
+    }
+
+    Mat old_frame, old_gray;
+    vector<Point2f> p0, p1;
+    // Take first frame and find corners in it
+    capture >> old_frame;
+
+    cvtColor(old_frame, old_gray, COLOR_BGR2GRAY);
+    goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+    // Create a mask image for drawing purposes
+    Mat mask = Mat::zeros(old_frame.size(), old_frame.type());
+
+    while (true) 
+    {
+
+        Mat frame, frame_gray;
+        capture >> frame;
+
+        if (frame.empty())
+            break;
+
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+        // calculate optical flow
+        vector<uchar> status;
+        vector<float> err;
+
+        TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
+        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(15, 15), 2, criteria);
+
+        vector<Point2f> good_new;
+        for (uint i = 0; i < p0.size(); i++)
+        {
+            // Select good points
+            if (status[i] == 1) {
+                good_new.push_back(p1[i]);
+                // draw the tracks
+                line(mask, p1[i], p0[i], colors[i], 2);
+                circle(frame, p1[i], 5, colors[i], -1);
+            }
+        }
+        Mat img;
+        add(frame, mask, img);
+        imshow("Frame", img);
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+        // Now update the previous frame and previous points
+        old_gray = frame_gray.clone();
+        p0 = good_new;
+    }
+}
+
+static void LB17_3()
+{
+
+}
+
 int main()
 {
     //LB12();
     //LB13();
     //LB14();
     //LB15();
-    LB16();
+    //LB16();
+
+    LB17_1();
+    //LB17_2();
+    //LB17_3();
 }
 
 
